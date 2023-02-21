4d18381fe509f0500394a394df5408f6202e0274
Vlad <vlad14196@gmail.com>
2023-02-21 12:58:16 +0300

    Initial Commit

---
+++ Flag/FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagApp.swift
+//  Flag
+//
+//  Created by user on 21.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
